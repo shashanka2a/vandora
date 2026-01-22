377532819e0a7b877d0768ebad25bd1e98d00ef3
shashank <jagannathamshashank@gmail.com>
2026-01-22 23:18:35 +0530

    Remove CaseStudySection and FinalCtaSection from HomePage component for a streamlined layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import HomePageMain from '@/features/home/ui'
-import CaseStudySection from '@/features/home/ui/CaseStudySection'
 import CloseLoopSection from '@/features/home/ui/CloseLoopSection'
-import FinalCtaSection from '@/features/home/ui/FinalCtaSection'
 import TilesSection from '@/features/home/ui/TilesSection'
 import VisualBg from '@/features/home/ui/VisualBg'
 import Header from '@/shared/ui/header'
@@ -18,9 +16,7 @@ const HomePage = () => {
 				</div>
 			</div>
 			<CloseLoopSection />
-			<CaseStudySection />
 			<TilesSection />
-			<FinalCtaSection />
 			<Footer />
 		</>
 	)
